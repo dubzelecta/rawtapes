c99bc9e1b1f1ddc4ea0ac7e4494a50c4a26d7b3f
david.timerman@sergata.com <david.timerman@sergata.com>
2014-06-23 02:39:16 +0300

    debugging artist page

--- application/views/artist.php
+++ application/views/artist.php
@@ -19,6 +19,8 @@
             <ul class="releases">
                 <?php
                 if(isset($artist_releases)){
+                    var_dump($artist_releases);
+                    die;
                 foreach ($artist_releases as $key => $artist_release) {
                     echo '<li><iframe style="border: 0; width: 270px; height: 270px;" 
                         src="http://bandcamp.com/EmbeddedPlayer/album='. $artist_release['bandcamp_link'] . '/size=large/bgcol=ffffff/linkcol=0687f5/minimal=true/transparent=true/" 
