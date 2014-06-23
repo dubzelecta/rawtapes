25387da0391a1baa0f862c9a7cf8cf3e0b68a648
david.timerman@sergata.com <david.timerman@sergata.com>
2014-06-23 03:29:52 +0300

    changes to main template

--- application/views/templates/main.php
+++ application/views/templates/main.php
@@ -4,10 +4,11 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=1003" />
 	<meta name="MobileOptimized" content="1003" />
-	<title>RawTapes</title>
+	<title>Raw Tapes Records</title>
 	<link href="/assets/css/all.css" media="all" type="text/css" rel="stylesheet">
 	<script type="text/javascript" src="/assets/js/jquery-1.8.3.min.js"></script>
 	<script type="text/javascript" src="/assets/js/jquery.main.js"></script>
+        <link rel="icon" type="image/png" href="/assets/images/logo.png">
 	<!--[if IE]><script type="text/javascript" src="js/ie.js"></script><![endif]-->
 </head>
 <body class="<?php 
