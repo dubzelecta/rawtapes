78a8fb5847c88fa24a2c638c83a4833ba39cb1a3
david.timerman@sergata.com <david.timerman@sergata.com>
2014-10-03 21:40:38 +0200

    events order

--- application/models/events_model.php
+++ application/models/events_model.php
@@ -10,7 +10,7 @@ class events_Model extends CI_Model {
         $this->db->select();
         $this->db->from('events');
         $this->db->where('event_date >',date("Y-m-d h:i:s"));
-        $this->db->order_by("event_date", "desc");
+        $this->db->order_by("event_date", "asc");
         $query = $this->db->get();
         $result = $query->result_array();
         if (!empty($result)) {
