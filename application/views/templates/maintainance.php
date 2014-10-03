a1209f18196db5b16765cb3747532f00edba5338
david.timerman@sergata.com <david.timerman@sergata.com>
2014-10-03 19:12:47 +0200

    new maintainance files

---
+++ application/views/templates/maintainance.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html>
+    <?php $this->load->view('head'); ?>
+    <body>
+        <style>
+            .maintainance_logo{
+                margin-left: 50%;
+                margin-right: auto;
+                top: 80px;
+                position: relative;
+            }
+            .maintainance_text{
+                margin-left: 40%;
+                margin-right: auto;
+                top: 100px;
+                position: relative;
+                width:100%;
+                float:left;
+                font-weight:bold;
+
+            }
+        </style>
+        <div class="maintainance_logo">
+
+            <img src="../assets/images/logo.png" />
+        </div>
+        <span class="maintainance_text">- N E W - W E B S I T E - C O M I N G - S O O N -</span>
+    </body>
+</html>
